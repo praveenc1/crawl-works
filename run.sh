6630fa8420dd4cab8a17339e4c3a15386a63c709
praveenc1 <praveen.cherukuri@gmail.com>
2024-12-18 10:27:04 -0500

    removed the hardcoding of data file

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-RUST_LOG=info cargo run --release -- -s ../data/CC-MAIN-20241013061417-20241013091417-00064.warc.gz
+RUST_LOG=info cargo run --release -- -s $(find ../data -name 'CC*.gz' | head -n 1)
